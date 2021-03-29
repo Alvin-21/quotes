2cb6c6686040bd9586cdddd03f3aacc79c2bbd3d
Alvin Awuor <alvinawuor8@gmail.com>
2021-03-29 12:46:20 +0300

    Move quote array to QuoteComponent class.

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes: Quote[] = [
+    {quote: "The greatest glory in living lies not in never falling, but in rising every time we fall", author: "Nelson Mandela", publisher: "Alvin"},
+    {quote: "The way to get started is to quit talking and begin doing", author: "Walt Disney", publisher: "Jane"}
+  ];
 
   constructor() { }
 
