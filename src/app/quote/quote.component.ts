52b6a192453f874dec266c3e41bea9ad0577cdd6
Alvin Awuor <alvinawuor8@gmail.com>
2021-03-29 13:42:15 +0300

    Restructure quotes in the quotes array.

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,8 +9,8 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    {quote: "The greatest glory in living lies not in never falling, but in rising every time we fall", author: "Nelson Mandela", publisher: "Alvin"},
-    {quote: "The way to get started is to quit talking and begin doing", author: "Walt Disney", publisher: "Jane"}
+   new Quote("The greatest glory in living lies not in never falling, but in rising every time we fall", "Nelson Mandela", "Alvin"),
+   new Quote("The way to get started is to quit talking and begin doing", "Walt Disney", "Jane")
   ];
 
   constructor() { }
