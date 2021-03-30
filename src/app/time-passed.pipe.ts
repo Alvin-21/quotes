4af26db3d6368e89824510376635d2870db59417
Alvin Awuor <alvinawuor8@gmail.com>
2021-03-30 03:11:18 +0300

    Add logic for time passed.

--- src/app/time-passed.pipe.ts
+++ src/app/time-passed.pipe.ts
@@ -5,8 +5,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimePassedPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
+  transform(value: any): string {
+    let today: Date = new Date();
+    let todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    var timeDifference = Math.abs(todayWithNoTime - value);
+    var timeDifferenceSeconds = timeDifference * 0.001;
 
+    return timeDifferenceSeconds + "s";
+  }
 }
